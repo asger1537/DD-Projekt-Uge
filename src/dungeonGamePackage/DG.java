f2727c2b57d09a7772e6ecbdba54b35cebe39d0e
asger1537 <34832830+asger1537@users.noreply.github.com>
2019-09-16 11:30:26 +0200

    Intial Commit - created project for Processing in Java

---
+++ src/dungeonGamePackage/DG.java
@@ -0,0 +1,23 @@
+package dungeonGamePackage;
+
+import processing.core.PApplet;
+
+public class DG extends PApplet{
+		
+	public static void main(String[] args) {
+		PApplet.main("dungeonGamePackage.DG");
+	}
+
+	public void settings() {
+		size(800, 800);
+	}
+
+	public void setup() {
+
+	}
+
+	public void draw() {
+  
+	}
+
+}

d81ffca4a67a0dbba51d90e89ca38c01b1fce412
Seisision <36736153+Seisision@users.noreply.github.com>
2019-09-17 14:44:37 +0200

    #25 Added checkAggroRange
    Added checkAggroRange

--- src/dungeonGamePackage/Enemy.java
+++ src/dungeonGamePackage/Enemy.java
@@ -1,14 +1,22 @@
 package dungeonGamePackage;
 
-abstract class Enemy extends MovingUnit{
+import processing.core.PVector;
+
+abstract class Enemy extends MovingUnit {
     float agroRange;
     int dmg;
-    int atkcd;//attack cooldown
+    int atkcd;// attack cooldown
     int expReward;
     Player target;
 
-    public void update(){
-        
+    public void update() {
+
     }
-    
+    void checkAggroRange(Player p) {
+        target = p;
+        float dist = PVector.dist(position, target.position);
+        if (dist >= agroRange) {
+            targetPosition = target.position;
+        }
+    }
 }

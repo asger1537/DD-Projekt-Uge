228468508dd19d4faada5bed660cc66e205c401a
ulriku <ulrikhlp@gmail.com>
2019-09-19 14:21:28 +0200

    #40 spawning of enemies moved from Utillity to Zone
    Since zone now handles spawning and keeping track of enemies some methods have been moved from utillity to Zone.
    
    spawnEnemyCluster
    spawnZoneEnemies
    spawnBoss
    
    have now been removed from Utillity

--- src/dungeonGamePackage/Utility.java
+++ src/dungeonGamePackage/Utility.java
@@ -14,44 +14,7 @@ class Utility {
         if (x > 0) return 1;
         return 0;
     }
-
-    static void spawnEnemyCluster(PVector position) {
-        int numEnemies = DG.floor(DG.random(3, 6));
-        PVector spawnVector = new PVector(30, 0);
-
-        for (int i = 0; i < numEnemies; i++) {
-            PVector spawnPosition;
-
-            // the first enemy is just spawned at the position, but the subsequent enemies
-            // are spawned around
-            // the first enemy with some randomness
-            if (i == 0) {
-                spawnPosition = position;
-            } else {
-                spawnPosition = PVector.add(position, spawnVector);
-            }
-            spawnVector.rotate(2 * DG.PI / numEnemies * DG.random(0.8f, 1.2f));
-            DG.enemies.add(new MeleeEnemy(spawnPosition));
-        }
-    }
-
-    static void spawnZoneEnemies() {
-        int numEnemyPacks = DG.floor(DG.random(8, 12));
-        PVector spawnPosition;
-        int maxDistance;
-        
-        for (int i = 0; i < numEnemyPacks; i++) {
-        int x = (int)DG.random(10,DG.width-10);
-        int y = (int)DG.random(DG.height/4,DG.height-DG.height/4);
-        spawnPosition = new PVector(x,y);
-        spawnEnemyCluster(spawnPosition);
-        }
-    }
-
-    static void spawnBoss(PVector spawnPosition) {
-        DG.enemies.add(new BossEnemy(spawnPosition));
-    }
-
+    
     static boolean contains(String[] strings, String s) {
         for (int i = 0; i < strings.length; i++) {
             if (strings[i] == s)

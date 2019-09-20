af3b457d967c031dd46387cf087a13dce953c5b2
ulriku <ulrikhlp@gmail.com>
2019-09-20 07:46:33 +0200

    #63 Added RangedEnemy class and added them to packs
    When a mob is spawned in a pack it now has a mobTypeID
    
    a mobpack is now made up of both melee and ranged mobs (although a pack can spawn without any ranged or melee depending on luck)

---
+++ src/dungeonGamePackage/RangedEnemy.java
@@ -0,0 +1,54 @@
+package dungeonGamePackage;
+
+import processing.core.PVector;
+import static dungeonGamePackage.DungeonGame.DG;
+
+class RangedEnemy extends Enemy {
+
+    RangedEnemy(PVector position) {
+        this.position = position;
+        lvl = DG.zone.currentZone;
+        color = DG.color(221, 33, 6);
+        msBase = 3f;
+        msCurrent = msBase;
+        radius = 10;
+        dead = false;
+        maxHp = 50+50*lvl;
+        hp = maxHp;
+        agroRange = 200;
+        dmg = 10*lvl;
+        atkcd = 60/lvl;
+        atkcdCurrent = 0;
+        expReward = 10*lvl;
+        healthBarLength = 30;
+    }
+
+    @Override
+    public void update() {
+        display();
+        checkAggro();
+        if (target != null) {
+            targetPosition = target.position;
+            if (atkcdCurrent > 1) {
+                atkcdCurrent--;
+            } else {
+                setlookDirection();
+                attack();
+            }
+        }
+    }
+
+    void setlookDirection() {
+        lookDirection = PVector.sub(targetPosition, position).normalize();
+    }
+
+    void attack() {
+        DG.projectiles.add(new Projectile(PVector.add(position, PVector.mult(lookDirection, radius + 10)),
+                PVector.mult(lookDirection, 10), 40, new String[] { "Player" }, 5, DG.color(206, 43, 34)));
+        atkcdCurrent = atkcd;
+    }
+
+    void onDeath() {
+        DG.p.expGet(expReward);
+    }
+}
--- src/dungeonGamePackage/Zone.java
+++ src/dungeonGamePackage/Zone.java
@@ -22,24 +22,24 @@ class Zone {
         levelCompleted = false;
     }
 
-    void display(){
+    void display() {
         DG.background(0);
-        
-        for (int y = 0; y < height; y += 50){
-            for (int x = 0; x < width; x += 50){
-                DG.image(DG.floorTile, x , y);
+
+        for (int y = 0; y < height; y += 50) {
+            for (int x = 0; x < width; x += 50) {
+                DG.image(DG.floorTile, x, y);
             }
         }
-        
+
     }
 
-
     void spawnEnemyCluster(PVector position) {
-        int numEnemies = DG.floor(DG.random(3,6));
+        int numEnemies = DG.floor(DG.random(3, 6));
         PVector spawnVector = new PVector(30, 0);
 
         for (int i = 0; i < numEnemies; i++) {
             PVector spawnPosition;
+            int mobType = DG.floor(DG.random(0, 2));
 
             // the first enemy is just spawned at the position, but the subsequent enemies
             // are spawned around
@@ -50,12 +50,16 @@ class Zone {
                 spawnPosition = PVector.add(position, spawnVector);
             }
             spawnVector.rotate(2 * DG.PI / numEnemies * DG.random(0.8f, 1.2f));
-            enemies.add(new MeleeEnemy(spawnPosition));
+            if (mobType == 0) {
+                enemies.add(new MeleeEnemy(spawnPosition));
+            } else {
+                enemies.add(new RangedEnemy(spawnPosition));
+            }
         }
     }
 
     void spawnZoneEnemies() {
-        int numEnemyPacks = DG.floor(DG.random(1*currentZone, 2*currentZone));
+        int numEnemyPacks = DG.floor(DG.random(1 * currentZone, 2 * currentZone));
         PVector spawnPosition;
         int maxDistance = 250;
 
@@ -63,12 +67,12 @@ class Zone {
             int x = (int) DG.random(10, DG.width - 10);
             int y = (int) DG.random(DG.height / 4, DG.height - DG.height / 4);
             spawnPosition = new PVector(x, y);
-            for(int j = 0; j < enemies.size(); j++){
+            for (int j = 0; j < enemies.size(); j++) {
                 int maxTries = 100;
-                while(maxTries > 0 && PVector.dist(spawnPosition, enemies.get(j).position) < maxDistance){
-                     x = (int) DG.random(10, DG.width - 10);
-                     y = (int) DG.random(DG.height / 4, DG.height - DG.height / 4);
-                    spawnPosition = new PVector(x, y); 
+                while (maxTries > 0 && PVector.dist(spawnPosition, enemies.get(j).position) < maxDistance) {
+                    x = (int) DG.random(10, DG.width - 10);
+                    y = (int) DG.random(DG.height / 4, DG.height - DG.height / 4);
+                    spawnPosition = new PVector(x, y);
                     --maxTries;
                 }
             }
@@ -87,10 +91,10 @@ class Zone {
             spawnBoss(new PVector(DG.width / 2, DG.height / 5));
             bossHasSpawned = true;
         }
-        if(levelCompleted){
+        if (levelCompleted) {
             portalTonextLevel();
         }
-        if(enemies.size() == 0 && bossHasSpawned){
+        if (enemies.size() == 0 && bossHasSpawned) {
             levelCompleted = true;
         }
         for (int i = 0; i < enemies.size(); i++) {
@@ -102,18 +106,19 @@ class Zone {
         }
     }
 
-    void generateNewZone(){
+    void generateNewZone() {
         currentZone += 1;
         bossHasSpawned = false;
         levelCompleted = false;
-        DG.p.position = new PVector(DG.width / 2f, DG.height * 7f / 8f); // player is moved to middle bottom of the screen when a new level starts
+        DG.p.position = new PVector(DG.width / 2f, DG.height * 7f / 8f); // player is moved to middle bottom of the
+                                                                         // screen when a new level starts
         spawnZoneEnemies();
     }
 
-    void portalTonextLevel(){
+    void portalTonextLevel() {
         int portalRadius = 100;
-        PVector portalPosition = new PVector(DG.width/2,DG.height/5);
-        DG.fill(0,0,255);
+        PVector portalPosition = new PVector(DG.width / 2, DG.height / 5);
+        DG.fill(0, 0, 255);
         DG.imageMode(DG.CENTER);
         DG.image(DG.portal, portalPosition.x, portalPosition.y);
         if (PVector.dist(portalPosition, DG.p.position) < portalRadius + DG.p.radius) {
@@ -122,6 +127,7 @@ class Zone {
 
         DG.textAlign(DG.CENTER);
         DG.fill(0);
-        DG.text(String.format("ZONE %d UNLOCKED", currentZone + 1), portalPosition.x, portalPosition.y - portalRadius*1.2f);
+        DG.text(String.format("ZONE %d UNLOCKED", currentZone + 1), portalPosition.x,
+                portalPosition.y - portalRadius * 1.2f);
     }
 }

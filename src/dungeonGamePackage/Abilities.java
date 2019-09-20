b26fde6de922c56407344b2234b57c5a7fc3a221
asger1537 <34832830+asger1537@users.noreply.github.com>
2019-09-20 13:05:28 +0200

    #73 Addded work on chainLightning

--- src/dungeonGamePackage/Abilities.java
+++ src/dungeonGamePackage/Abilities.java
@@ -1,10 +1,27 @@
 package dungeonGamePackage;
 
 //A collection of the ability anonymous classes 
+import static dungeonGamePackage.DungeonGame.DG;
+
+import processing.core.PVector;
 
 class Abilities{
-    Ability chainLightning = new Ability(1.5f, 10){
+    Ability chainLightningShot = new Ability(1.5f, 10){
+        @Override
+        void use(){
+            DG.projectiles.add(new Projectile(PVector.add(DG.p.position, DG.p.barrelLongSide), DG.p.lookDirection, (int)(scaling*DG.p.dmg),
+             new String[]{"Enemy"}, 10, DG.color(30)){
+                int numChains = 3;
+                int chainCount = 0;
+                @Override
+                void onEnemyCollision(Enemy e){
+                    e.hit = true;
+                    chainCount++;
+                    
+                }
 
+             });
+        }
     };
 
 
